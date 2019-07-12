18f903dcded81737a41419b02d62ba3f02ba59b8
paulalm94 <almasan@ac.upc.edu>
2019-07-12 15:07:53 +0200

    Predict option is removed because its not implemented

--- Code/run_routenet.sh
+++ Code/run_routenet.sh
@@ -16,11 +16,6 @@ if [[ "$1" = "train_multiple" ]]; then
     python3 routenet_with_link_cap.py train --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=16,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8"  --train  /home/datasets/SIGCOMM/$2/tfrecords/train/*.tfrecords /home/datasets/SIGCOMM/$3/tfrecords/train/*.tfrecords --train_steps $5 --eval_ /home/datasets/SIGCOMM/geant2bw/tfrecords/evaluate/*.tfrecords /home/datasets/SIGCOMM/geant2bw/tfrecords/train/*.tfrecords --shuffle_buf 30000 --model_dir ./CheckPoints/$4
 fi
 
-if [[ "$1" = "predict" ]]; then
-
-    python3 routenet_with_link_cap.py predict --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=16,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8" --model_dir=./CheckPoints/$2 --eval_steps=$3 --predict /home/datasets/SIGCOMM/$4/tfrecords/evaluate/*.tfrecords
-fi
-
 if [[ "$1" = "normalize" ]]; then
 
     python3 normalize.py --dir /home/datasets/SIGCOMM/nsfnetbw/tfrecords/train/ /home/datasets/SIGCOMM/nsfnetbw/tfrecords/evaluate/ /home/datasets/SIGCOMM/synth50bw2/tfrecords/evaluate/ /home/datasets/SIGCOMM/synth50bw2/tfrecords/train/ --ini configNSFNET50.ini
