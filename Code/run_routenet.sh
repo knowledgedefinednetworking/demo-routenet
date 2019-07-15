0338bc6a4bbe58ddd42fceb40f15de071b3ff406
paulalm94 <almasan@ac.upc.edu>
2019-07-15 16:39:23 +0200

    Default hyperparameters changed

--- Code/run_routenet.sh
+++ Code/run_routenet.sh
@@ -7,13 +7,13 @@ fi
 
 if [[ "$1" = "train" ]]; then
 
-    python3 routenet_with_link_cap.py train --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=16,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8"  --train  /home/datasets/SIGCOMM/$2/tfrecords/train/*.tfrecords --train_steps $3 --eval_ /home/datasets/SIGCOMM/$2/tfrecords/evaluate/*.tfrecords --model_dir ./CheckPoints/$2
+    python3 routenet_with_link_cap.py train --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=32,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8"  --train  /home/datasets/SIGCOMM/$2/tfrecords/train/*.tfrecords --train_steps $3 --eval_ /home/datasets/SIGCOMM/$2/tfrecords/evaluate/*.tfrecords --model_dir ./CheckPoints/$2
 
 fi
 
 if [[ "$1" = "train_multiple" ]]; then
 
-    python3 routenet_with_link_cap.py train --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=16,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8"  --train  /home/datasets/SIGCOMM/$2/tfrecords/train/*.tfrecords /home/datasets/SIGCOMM/$3/tfrecords/train/*.tfrecords --train_steps $5 --eval_ /home/datasets/SIGCOMM/geant2bw/tfrecords/evaluate/*.tfrecords /home/datasets/SIGCOMM/geant2bw/tfrecords/train/*.tfrecords --shuffle_buf 30000 --model_dir ./CheckPoints/$4
+    python3 routenet_with_link_cap.py train --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=32,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8"  --train  /home/datasets/SIGCOMM/$2/tfrecords/train/*.tfrecords /home/datasets/SIGCOMM/$3/tfrecords/train/*.tfrecords --train_steps $5 --eval_ /home/datasets/SIGCOMM/geant2bw/tfrecords/evaluate/*.tfrecords /home/datasets/SIGCOMM/geant2bw/tfrecords/train/*.tfrecords --shuffle_buf 30000 --model_dir ./CheckPoints/$4
 fi
 
 if [[ "$1" = "normalize" ]]; then
